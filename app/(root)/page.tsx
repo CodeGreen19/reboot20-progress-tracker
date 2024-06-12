ce8466c1d12c919285fa197c10b9d50764f3a692
codegreen <codegreen19s@gmail.com>
2024-06-12 11:27:00 +0600

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad sequi
+      molestiae, deserunt nemo, facere aliquid excepturi, ipsam aperiam
+      consectetur explicabo fugiat maiores exercitationem voluptas labore modi
+      veritatis possimus officiis libero. <Button children="click me" />
+    </div>
+  );
+};
+
+export default HomePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSand = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-sand",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sand antialiased",
+          fontSand.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
