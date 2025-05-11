a1d00f438fe634592a53a0709ecdfd1422f64c08
codegreen <codegreen19s@gmail.com>
2025-05-11 22:28:52 +0600

    testing

---
+++ app/(root)/testpage/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <div>
+      Test page: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+      veritatis inventore consequatur magni fugiat amet deleniti consectetur ab,
+      unde quam enim odio! Eos, sint. Blanditiis accusantium impedit natus eaque
+      illum.
+    </div>
+  );
+};
+
+export default TestPage;
